6f93fc517d433871102c7ae9c1765ded2ce40c8c
huhinka <huhinka@163.com>
2025-03-28 21:38:51 +0800

    add server app

--- packages/server/eslint.config.mjs
+++ packages/server/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
+import eslintPluginImport from "eslint-plugin-import";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
@@ -9,5 +10,24 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], // 指定适用的文件类型
+    plugins: {
+      import: eslintPluginImport, // 启用 import 插件
+    },
+    rules: {
+      // 强制导入模块时必须带上文件后缀
+      "import/extensions": [
+        "error", // 将规则设置为错误级别
+        "always", // 始终要求文件后缀
+        {
+          js: "always", // 对于 .js 文件，强制要求后缀
+          jsx: "always", // 对于 .jsx 文件，强制要求后缀
+          ts: "always", // 对于 .ts 文件，强制要求后缀
+          tsx: "always", // 对于 .tsx 文件，强制要求后缀
+        },
+      ],
+    },
   },
 ]);
