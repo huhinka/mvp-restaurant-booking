03804ff8fb82a379a97960cbcdb9e594cb819d90
huhinka <huhinka@163.com>
2025-04-04 18:43:20 +0800

    refact(server): narrow the scope of try catch in register router

--- apps/server/src/auth/auth.route.js
+++ apps/server/src/auth/auth.route.js
@@ -9,38 +9,37 @@ import { loginValidator, registerValidator } from "./validators.js";
 export const router = express.Router();
 
 router.post("/register", registerValidator, async (req, res) => {
+  const { email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  let user;
   try {
-    const { email, password } = req.body;
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const user = await User.create({
+    user = await User.create({
       email,
       password: hashedPassword,
       // 员工后台添加，不走注册接口
       role: "guest",
     });
-
-    const token = jwt.sign(
-      { userId: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      // TODO 添加刷新 token
-      { expiresIn: "8h" },
-    );
-
-    res.status(201).json({
-      userId: user._id,
-      role: user.role,
-      token,
-    });
   } catch (err) {
-    // TODO 这个异常检测应该在上面 User.create 上处理，而不是全局
     if (err instanceof MongoError && err.code === 11000) {
       throw new AuthError("该邮箱或手机号已注册", 400);
     }
-
     throw err;
   }
+
+  const token = jwt.sign(
+    { userId: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    // TODO 添加刷新 token
+    { expiresIn: "8h" },
+  );
+
+  res.status(201).json({
+    userId: user._id,
+    role: user.role,
+    token,
+  });
 });
 
 router.post("/login", loginValidator, async (req, res) => {
