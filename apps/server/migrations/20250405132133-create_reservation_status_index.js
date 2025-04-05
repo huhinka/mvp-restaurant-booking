1920c3dcf90ff538428cc527d7adbce56c7da262
huhinka <huhinka@163.com>
2025-04-05 21:24:44 +0800

    feat(server): add reservation status index

---
+++ apps/server/migrations/20250405132133-create_reservation_status_index.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+const indexName = "status_index";
+/**
+ * @param db {import('mongodb').Db}
+ * @param client {import('mongodb').MongoClient}
+ * @returns {Promise<void>}
+ */
+export const up = async (db, client) => {
+  // TODO write your migration here.
+  // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+  // Example:
+  // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+  await db
+    .collection("reservations")
+    .createIndex({ status: 1 }, { name: indexName });
+};
+
+/**
+ * @param db {import('mongodb').Db}
+ * @param client {import('mongodb').MongoClient}
+ * @returns {Promise<void>}
+ */
+export const down = async (db, client) => {
+  // TODO write the statements to rollback your migration (if possible)
+  // Example:
+  // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+  await db.collection("users").dropIndex(indexName);
+};
