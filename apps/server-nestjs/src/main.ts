478d211ff4443c9815e0049af8ef1704baf21f20
huhinka <huhinka@163.com>
2025-04-11 11:46:25 +0800

    feat(server-nestjs): read port config from .env

--- apps/server-nestjs/src/main.ts
+++ apps/server-nestjs/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new AppExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3030);
 }
 bootstrap();
